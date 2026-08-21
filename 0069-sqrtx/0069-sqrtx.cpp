fe4fff81f0be7cc45da785c56a823a6de65db3d2
aryanr2708-svg <aryanr2708@gmail.com>
2026-08-21 23:58:24 +0530

    Time: 2 ms (20.39%), Space: 8.6 MB (51.22%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -2,9 +2,13 @@ class Solution {
 public:
     int mySqrt(int x) {
         int i=0;
-        while((long)i*i<=x){
+        if(x==0){
+            return 0;
+        }else{
+            while((long)i*i<=x){
             i++;
         }
         return (i-1);
+        }
     }
 };
