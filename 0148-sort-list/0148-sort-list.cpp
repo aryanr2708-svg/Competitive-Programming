2a425270c5dd708871ba933dae838ab116ec8df1
Aryan Roy <aryanr2708@gmail.com>
2026-09-16 21:30:54 +0530

    Time: 58 ms (10.75%), Space: 75.6 MB (31.9%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -11,18 +11,44 @@
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
-        vector<int>ar;
-        ListNode* temp=head;
-        while(temp!=NULL){
-            ar.push_back(temp->val);
-            temp=temp->next;
+        if(head==NULL || head->next==NULL){
+            return head;
         }
-        sort(ar.begin(),ar.end());
-        temp=head;
-        for(int i=0;i<ar.size();i++){
-            temp->val=ar[i];
-            temp=temp->next;
+        ListNode* temp=NULL;
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=NULL && fast->next!=NULL){
+            temp=slow;
+            slow=slow->next;
+            fast=fast->next->next;
         }
-        return head;
+        temp->next=NULL;
+        ListNode* left = sortList(head);
+        ListNode* right = sortList(slow);
+        return msort(left,right);
+    }
+    ListNode* msort(ListNode* left,ListNode* right){
+        ListNode* t= new ListNode(0);
+        ListNode* cur=t;
+        while(left!=NULL && right!=NULL){
+            if(left->val <= right->val){
+                cur->next=left;
+                left=left->next;
+            }
+            else{
+                cur->next=right;
+                right=right->next;
+            }
+        cur=cur->next;
+        }
+        if(left!=NULL){
+            cur->next=left;
+            left=left->next;
+        }
+        if(right!=NULL){
+            cur->next=right;
+            right=right->next;
+        }
+    return t->next;
     }
 };
