825fd6dce87cae282f98a11bb72ff532e7869764
Aryan Roy <aryanr2708@gmail.com>
2026-09-20 17:28:23 +0530

    Time: 0 ms (100%), Space: 9.8 MB (47.88%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int sum=0;
+        int i=1;
+        for(char c:s){
+            sum+=(26-(c-'a'))*i;
+            i++;
+        }
+        return sum;
+    }
+};
